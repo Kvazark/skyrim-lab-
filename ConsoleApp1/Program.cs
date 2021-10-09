1d22cb9ed440af5dd0e8cd2025fe8cf745185d62
first <Katya_Klebleeva@mail.ru>
2021-10-09 15:58:16 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creature
+{
+ //Класс существа
+    class Creature
+    {
+// Поля класса
+        public string name { get; set; }
+        public string species { get; set; }
+        public int age { get; set; }
+        public int height { get; set; }
+        public string eyeColor = "blue";
+
+        //Конструктор по умолчанию
+        public Creature()
+        {
+            name = "Geralt";
+            species = "elf";
+            age = 38;
+            height = 185;
+        }
+        //Конструктор копирования
+        public Creature(Creature obj)
+        {
+            this.name = obj.name;
+            this.species = obj.species;
+            this.age = obj.age;
+            this.height = obj.height;
+            this.eyeColor = obj.eyeColor;
+        }
+        //Конструктор с параметрами
+        public Creature(string name,string species,int age, int height)
+        {
+            this.name = name;
+            this.species = species;
+            this.age = age;
+            this.height = height;
+        }
+        // Открытый метод класса
+        public void Relax()
+        {
+            Console.WriteLine("The {0} is resting after riding.", name);
+        }
+
+        public void Reflect()
+        {
+            if(age > 0 && age < 14)
+            {
+                Console.WriteLine("The {0} wondered: 'Why a corgi has a peach instead of a priest?'", name);
+            }
+            else if(age >= 14 && age < 22)
+            {
+                Console.WriteLine("The {0} is thinking about how to solve urgent problems.", name);
+            }
+            else if (age >= 22 && age < 63)
+            {
+                Console.WriteLine("The {0} remembered the good moments.", name);
+            }
+            else if (age >= 63)
+            {
+                Console.WriteLine("The {0} is tired of living...", name);
+            }
+            else
+            {
+
+            }
+        }
+
+        public void Riding()
+        {
+            Random randGen = new Random();
+            int rndNum = randGen.Next(7);
+            if (rndNum == 2)
+            {
+                this.Fiasco();
+            }
+            else
+            {
+                Console.WriteLine("{0} is a good horseman!", name);
+            }
+        }
+        // Закрытый метод класса
+        private int Fiasco()
+        {
+            Console.WriteLine("The {0} fell from a horse.", name);
+            Random randGen = new Random();
+            int num = randGen.Next(1, 9);
+            return num;
+        }
+
+        public void EatSmth(int num)
+        {
+            Random gen = new Random();
+            int rand = gen.Next(15);
+            string food = "";
+            if(num == 1)
+            {
+                if (rand < 4)
+                {
+                    food = "gammon";
+                }
+                else if (rand > 4 && rand < 7)
+                {
+                    food = "chicken leg";
+                }
+                else if (rand > 7 && rand < 11)
+                {
+                    food = "duck";
+                }
+                else
+                {
+                    string[] vars = { "ham", "meat brisket", "bacon", "rabbit" };
+                    rand = gen.Next(vars.Length);
+                    food = vars[rand];
+                }
+                Console.WriteLine(food);
+            }
+            else if (num == 2)
+            {
+                if (rand < 4)
+                {
+                    food = "apple";
+
+                }
+                else if (rand > 4 && rand < 7)
+                {
+                    food = "raspberry";
+
+                }
+                else if (rand > 7 && rand < 11)
+                {
+                    food = "pear";
+
+                }
+                else
+                {
+
+                    string[] vars1 = { "cherry", "plum", "blackberry", "blueberry" };
+                    rand = gen.Next(vars1.Length);
+                    food = vars1[rand];
+                }
+                Console.WriteLine(food);
+            }
+            
+        }
+        public void EatSmth(int num, int count)
+        {
+            string[] foods = new string[count];
+            string food = "";
+
+            Console.Write("{0} ate today ", name);
+
+            for (int i=0; i < count; i++)   
+            {
+                Random gen = new Random();
+                int rand = gen.Next(10);
+
+                if (rand < 3)
+                {
+                    this.EatSmth(num);
+                    
+                    
+                }
+                else if (rand > 3 && rand < 5)
+                {
+                    this.EatSmth(num);
+
+                }
+                else if (rand > 5 && rand < 8)
+                {
+                    this.EatSmth(num);
+
+                }
+                else
+                {
+                  
+                    this.EatSmth(num);
+                }
+                foods[i] = food;
+
+            }
+            
+
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"This is a {species} {age}-year-old {name} {height} cm tall with {eyeColor} eyes.");
+        }
+        ~Creature()   
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("{0} dead", name);
+            Console.Beep();
+        }
+
+
+    }
+    class Program
+    {
+        protected static void Comparison(int h1, int h2)
+        {
+            if (h1 > h2)
+            {
+                Console.WriteLine("The first is higher than the second.");
+            }
+            else if (h1 < h2)
+            {
+                Console.WriteLine("The first is lower than the second.");
+            }
+            else
+            {
+                Console.WriteLine("They have the same height.");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Creature cr1 = new Creature();
+            cr1.GetInfo();
+
+            Creature cr2 = new Creature { name = "D'Alen", species = "kajit", age = 12, height = 156 };
+            cr2.GetInfo();
+            cr2.Reflect();
+            Console.WriteLine("Let's compare the growth {0} and {1}...", cr1.name, cr2.name);
+            Comparison(cr1.height,cr2.height);
+
+            Console.WriteLine("");
+
+            Creature cr4 = new Creature(cr1);
+            cr4.GetInfo();
+
+            Console.WriteLine("{0} prefers? Write number" + '\n' + "1.meat" + '\n' + "2.vegetation", cr4.name);
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Нow many times does {0} eat?", cr4.name);
+            int count = Convert.ToInt32(Console.ReadLine());
+            cr4.EatSmth(num2, count);
+
+            Creature cr3 = new Creature();
+            Console.WriteLine("What is the name of the creature?");
+            cr3.name = Console.ReadLine();
+            Console.WriteLine("What race is {0} ?", cr3.name);
+            cr3.species = Console.ReadLine();
+            Console.WriteLine("How old is {0} ?", cr3.name);
+            cr3.age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0} has {1} eyes", cr3.name, cr3.eyeColor);
+
+            cr3.Riding();
+            cr3.Relax();
+
+            Console.WriteLine("{0} prefers? Write number"+ '\n' +"1.meat" + '\n'+ "2.vegetation", cr3.name);
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("{0} ate ", cr3.name);
+            cr3.EatSmth(num1);
+            cr3.Reflect();
+
+            Creature[] obj = new Creature[3];
+            for (int i = 0; i < 3; i++) 
+                obj[i] = new Creature();
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,400 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creature
+{
+    abstract class Creature
+    {
+        //Абстрактне свойства
+        public abstract string Name { get; set; }
+        public abstract string Sex { get; protected set; }
+        private int age { get; set; }
+        //Обыкновенное поле со свойством
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+        //Абстрактный метод без реавлизации
+        public abstract bool Hunt();
+        //Виртуальный метод
+        public virtual void Riding()
+        {
+            Random randGen = new Random();
+            int rndNum = randGen.Next(7);
+            if (rndNum == 2)
+            {
+                this.Fiasco();
+            }
+            else
+            {
+                Console.WriteLine("{0} is a good horseman!", Name);
+            }
+        }
+        
+        private int Fiasco()
+        {
+            Console.WriteLine("The {0} fell from a horse.", Name);
+            Random randGen = new Random();
+            int num = randGen.Next(1, 9);
+            return num;
+        }
+        //Обыкновенный метод
+        public virtual void InfOutput()
+        {
+            Console.WriteLine("My name is {0} I am a {1}-year-old {2}.",Name, Age, Sex);
+        }
+        
+
+    }
+    //Производный класс от класса Существо- класс Каджит
+    class Kajit : Creature
+    {
+        //Новое свойство
+        public string CoatColor { get; set; }
+        //Реализация абстрактынх свойств базового класса
+        private string sex;
+        public override string Sex
+        {
+            get
+            {
+                return sex;
+            }
+            protected set
+            {
+                if (string.Compare(value, "man", true) == 0 ||
+                    string.Compare(value, "woman", true) == 0)
+                {
+                    sex = value;
+                }
+            }
+        }
+                    
+        public override string Name { get; set; }
+        public Kajit(string name, string sex, int age, string coatColor) : base()
+        {
+            Name = name;
+            Sex = sex;
+            Age = age;
+            CoatColor = coatColor;
+        }
+           
+        
+        //Реализация абстрактного метода базового класса
+        public override bool Hunt()
+        {
+            var randomGenerator = new Random();
+            int randInt = randomGenerator.Next(3);
+            if (randInt == 0)
+            {
+                Console.WriteLine("{0} killed a wild boar", Name);
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("{0} couldn't catch anyone on the hunt.", Name);
+                return true;
+            }
+        }
+        //Новый метод
+        public void Steal()
+        {
+            Random gen = new Random();
+            int rand = gen.Next(50);
+            string thing = "";
+            if (rand < 23)
+            {
+                thing = "detected";
+            }
+            else if (rand > 23 && rand < 38)
+            {
+                thing = "note";
+            }
+            else if (rand > 38 && rand < 47)
+            {
+                thing = "coin";
+            }
+            else
+            {
+                string[] vars = { "necklace", "ring", "sapphire", "the soul stone", "the dagger", "book" };
+                rand = gen.Next(vars.Length);
+                thing = vars[rand];
+            }
+            if (rand < 23)
+            {
+                Console.WriteLine("{0} didn't steal anything." ,Name);
+            }
+            else
+            {
+                Console.WriteLine("{0} stol a " + thing, Name);
+            }
+            
+            
+        }
+        public void Growl()
+        {
+            Console.WriteLine("{0}: 'R-r-r'"+'\n'+ "*The {1} fur stood on end*", Name, CoatColor);
+            Console.Beep();
+        }
+        //Новый метод, переопределяющий метод базового класса Creature
+        public override void Riding()
+        {
+            Random randGen = new Random();
+            int rndNum = randGen.Next(7);
+            if (rndNum != 2)
+            {
+                Console.WriteLine("{0} couldn't saddle the horse and said: 'Kajit doesn't like this horse, Kajit would rather eat it.'", Name);
+            }
+            else
+            {
+                Console.WriteLine("{0} is a good horseman!", Name);
+            }
+        }
+
+    }
+    class Ogre : Creature
+    {
+        //Новое свойство
+        public int CaninesSize { get; set; }
+        //Реализация абстрактынх свойств базового класса
+        private string sex;
+        public override string Sex
+        {
+            get
+            {
+                return sex;
+            }
+            protected set
+            {
+                if (string.Compare(value, "man", true) == 0 ||
+                    string.Compare(value, "woman", true) == 0)
+                {
+                    sex = value;
+                }
+            }
+        }
+        public Ogre(string name, string sex, int age, int caninesSize) : base()
+        {
+            Name = name;
+            Sex = sex;
+            Age = age;
+            CaninesSize = caninesSize;
+        }
+        public override string Name { get; set; }
+        //Реализация абстрактного метода базового класса
+        public override bool Hunt()
+        {
+            var randomGenerator = new Random();
+            int randInt = randomGenerator.Next(3);
+            if (randInt == 0)
+            {
+                Console.WriteLine("{0} killed a wild bear.", Name);
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("{0} couldn't catch anyone on the hunt.", Name);
+                return true;
+            }
+        }
+        public void ThrowingStones()
+        {
+            Console.WriteLine("{0} threw {1} stones, because the length of his fangs is so much cm", Name, CaninesSize);
+        }
+    }
+    class Elf : Creature
+    {
+        //Новое свойство
+        public string Occupation { get; set; }
+        //Реализация абстрактынх свойств базового класса
+        private string sex;
+        public override string Sex
+        {
+            get
+            {
+                return sex;
+            }
+            protected set
+            {
+                if (string.Compare(value, "man", true) == 0 ||
+                    string.Compare(value, "woman", true) == 0)
+                {
+                    sex = value;
+                }
+            }
+        }
+        public override string Name { get; set; }
+        public Elf(string name, string sex, int age, string occupation) : base()
+        {
+            Name = name;
+            Sex = sex;
+            Age = age;
+            Occupation = occupation;
+        }
+        //Реализация абстрактного метода базового класса
+        public override bool Hunt()
+        {
+            var randomGenerator = new Random();
+            int randInt = randomGenerator.Next(3);
+            if (randInt == 0)
+            {
+                Console.WriteLine("{0} killed a deer.", Name);
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("{0} couldn't catch anyone on the hunt.", Name);
+                return true;
+            }
+        }
+        //Новый метод
+        public virtual void CastASpell()
+        {
+            Random gen = new Random();
+            int rand = gen.Next(10);
+            string spell = "";
+            string[] spells = { "Arvak's Challenge", "Quick treatment", "Chain lightning"};
+            if (rand > 5)
+            {
+                Console.WriteLine("Now the {0} will conjure.", Name);
+                Console.WriteLine("And....");
+                Console.WriteLine("It's a spell {0}.", spell);
+            }
+            else
+            {
+                Console.WriteLine("Now the {0} will conjure.", Name);
+                Console.WriteLine("And....");
+                Console.WriteLine("{0} didn't succeed.", Name);
+            }
+        }
+        public override void InfOutput()
+        {
+            Console.WriteLine("Hi, my name is {0}, I am a {1} of {2} years old and I am {3}.", Name, Sex, Age, Occupation);
+        }
+
+
+    }
+    class ForestElf : Elf
+    {
+        //Новое свойство
+        public string Forest { get; set; }
+        public ForestElf(string name, string sex, int age, string occupation, string Forest) : base(name, sex, age, occupation)
+        {
+        }
+        
+        //Новый метод
+        public void MakeJoke()
+        {
+            Console.WriteLine("Once {0} says: 'Если моя собака прыгнула в воду, можно ли сказачать что она бульdog?'", Name);
+        }
+    }
+    class HighElf : Elf
+    {
+        //Новое свойство
+        public int height { get; set; }
+        public HighElf(string name, string sex, int age, string occupation, int height) : base(name, sex, age, occupation)
+        {
+        }
+
+        //Новый метод
+        public void MakeWeapons()
+        {
+            Random gen = new Random();
+            int rand = gen.Next(15);
+            string weapons = "";
+            string[] vars = { "sword", "spear", "bow", "tomahawk" };
+            rand = gen.Next(vars.Length);
+            weapons = vars[rand];
+            Console.WriteLine("{0} made a "+ weapons, Name);
+        }
+    }
+    class DarkElf : Elf
+    {
+        //Новое свойство
+        public string eyeColor { get; set; }
+        public DarkElf(string name, string sex, int age, string occupation, string eyeColor) : base(name, sex, age, occupation)
+        {
+        }
+        //Новый метод, переопределяющий метод CastASpell класса Elf
+        public override void CastASpell()
+        {
+            Random gen = new Random();
+            int rand = gen.Next(10);
+            string spell = "";
+            string[] spells = { "freezing", "firestorm", "chain lightning", "ice spike", "thunder wall" };
+            if (rand != 1 || rand !=8)
+            {
+                Console.WriteLine("{0} casts a spell of destruction.", Name);
+                rand = gen.Next(spells.Length);
+                spell = spells[rand];
+                Console.WriteLine("And....");
+                Console.WriteLine("Now the spell '{0}' has killed the robber.", spell);
+            }
+            else
+            {
+                Console.WriteLine("{0} casts a spell of destruction.", Name);
+                Console.WriteLine("And....");
+                Console.WriteLine("{0} didn't succeed.", Name);
+            }
+        }
+
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Creature kjCr = new Kajit("K'Amobus", "man", 25, "brown");
+            Creature elfCr = new Elf("Kella", "woman", 21, "hunter");
+            Creature ogreCr = new Ogre("Trall", "man", 43, 19);
+            Creature fstElfCr = new ForestElf("Nill", "man", 58, "the bard", "Oak forest");
+
+            kjCr.InfOutput();
+
+            elfCr.Riding();
+            elfCr.Hunt();
+
+            fstElfCr.Hunt();
+            fstElfCr.Riding();
+
+            ogreCr.Hunt();
+
+            Kajit kajit = new Kajit("D'Abobus", "man", 31, "black");
+            kajit.Steal();
+            kajit.Riding();
+            kajit.Growl();
+
+            Console.WriteLine("");
+
+            //Демонстация методов
+            Ogre ogre = new Ogre("Shrek", "man", 38, 15);
+            ogre.ThrowingStones();
+            
+            Elf elf = new Elf("Din", "man", 26, "hunter");
+            elf.CastASpell();
+
+            ForestElf foresrElf = new ForestElf("Nill", "man", 58, "the bard", "Oak forest");
+            foresrElf.MakeJoke();
+
+            HighElf highElf = new HighElf("Bon", "man", 32, "trader", 215);
+            highElf.MakeWeapons();
+
+            DarkElf darkElf = new DarkElf("Eva", "woman", 19, "magician", "red");
+            darkElf.InfOutput();
+            darkElf.CastASpell();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
